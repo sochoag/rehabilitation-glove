ec22bba71fa59c2f7a2d6d2cc0e65400cf83bc9a
Sochoag <santiagodav@hotmail.es>
2023-01-10 18:53:02 -0500

    Github Page created

--- docs/script.js
+++ docs/script.js
@@ -13,7 +13,7 @@ const options = {
 };
 
 // Connection
-const WebSocket_URL = "ws://sochoag.ga:8083/mqtt";
+const WebSocket_URL = "ws://sochoag.com:8083/mqtt";
 
 function connectMQTT() {
   let username = document.getElementById("username");
