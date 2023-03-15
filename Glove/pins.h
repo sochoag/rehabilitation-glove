34b40e506cac3dc1f7ead67e2f1f9bc0f591b449
Sochoag <santiagodav@hotmail.es>
2023-03-15 16:35:57 -0500

    Cleaning: Remove firmware files

--- Glove/pins.h
+++
@@ -1,8 +0,0 @@
-#ifndef pins_h
-#define pins_h
-  const byte flexPins[] = {36,39,34,35,32}; // Sensores Flex
-  const byte flexSize = sizeof(flexPins);
-
-  const byte phalangePins[] = {33,25,26,27}; // Sensores Falanges
-  const byte phalangeSize = sizeof(phalangePins);
-#endif
