92614d764de152d1e940c1ff8aa0e3d249cbb4e6
Sochoag <santiagodav@hotmail.es>
2023-03-21 10:51:34 -0500

    Analog Expander Added

--- Glove/pins.h
+++ Glove/pins.h
@@ -1,8 +1,13 @@
 #ifndef pins_h
 #define pins_h
-  const byte flexPins[] = {36,39,34,35,32}; // Sensores Flex
+  const byte flexPins[] = {0,2,3,6,8}; // Sensores Flex
   const byte flexSize = sizeof(flexPins);
 
-  const byte phalangePins[] = {33,25,26,27}; // Sensores Falanges
+  const byte phalangePins[] = {1,2,3,4}; // Sensores Falanges
   const byte phalangeSize = sizeof(phalangePins);
+
+  const byte selectorsPins[]={32,33,25,26};
+  const byte selectorsSize =  sizeof(selectorsPins);
+
+  const byte analogPin = 36;
 #endif
---
+++ Glove/secrets_example.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char *ssid = "YOUR_SSID";
+const char *password = "YOUR_PASSWORD";
+const char *mqtt_server = "YOUR_MQTT_BROKER";
+const char *mqtt_username = "YOUR_MQTT_USERNAME";
+const char *mqtt_password = "YOUR_MQTT_PASSWORD";
+
+#endif
