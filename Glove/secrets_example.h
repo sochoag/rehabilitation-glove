35b793010cc94fa189a6e58742e7bfef89868323
Sochoag <santiagodav@hotmail.es>
2023-04-29 13:33:38 -0500

    .gitignore added

--- Glove/secrets_example.h
+++ Glove/secrets_example.h
@@ -1,10 +1,10 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-const char *ssid = "YOUR_SSID";
-const char *password = "YOUR_PASSWORD";
-const char *mqtt_server = "YOUR_MQTT_BROKER";
-const char *mqtt_username = "YOUR_MQTT_USERNAME";
-const char *mqtt_password = "YOUR_MQTT_PASSWORD";
+const String ssid = "YOUR_SSID";
+const String password = "YOUR_PASSWORD";
+const String mqtt_server = "YOUR_MQTT_BROKER";
+const String mqtt_username = "YOUR_MQTT_USERNAME";
+const String mqtt_password = "YOUR_MQTT_PASSWORD";
 
 #endif
